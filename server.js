82addd3c6e143703c125336273a36356c039182d
Amey Parundekar <parundekaramey@gmail.com>
2017-03-15 17:17:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/sudocode',function(req,res){
+app.get('sudocode',function(req,res){
     res.sendFile(path.join(__dirname,'ui','sudocode.html'));
 });
 app.get('/ui/style.css', function (req, res) {
