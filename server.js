44766b0ae8d86d1fe9b45ce0a648328d8254657b
Amey Parundekar <parundekaramey@gmail.com>
2017-03-25 09:56:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database:	'the-lost-explorer',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'db-the-lost-explorer-26100'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
