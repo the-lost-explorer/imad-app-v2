b223874c2edc5fd4f64cc25eb8d75b626e93f17e
Amey Parundekar <parundekaramey@gmail.com>
2017-03-25 23:21:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,8 @@ app.post('/create-user',function(req,res){
 app.post('/login',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
+    console.log(req.body.username);
+    console.log(req.body.password);
      pool.query('SELECT * FROM "user" WHERE username = $1',[username],function(err,result){
        if(err){
            res.status(500).send(err.toString());
