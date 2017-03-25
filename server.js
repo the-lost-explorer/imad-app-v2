73e00264109054f458977c1a3331ce38683c9889
Amey Parundekar <parundekaramey@gmail.com>
2017-03-25 09:47:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:	'the-lost-explorer' ,  
+    database:	'the-lost-explorer',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password:	DB_PASSWORD
+};
+
+
+
+
+
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -19,6 +34,20 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+var pool = new pg.Pool(config);
+app.get('/test-db',function(req,res){
+   pool.query('SELECT*FROM test',function(req,res){
+       if(err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result));
+       }
+   });
+    
+});
+
+
 
 counter = 0;
 app.get('/counter',function(req,res){
