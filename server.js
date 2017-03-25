b4bd12af07271640000512fd5699bf962b24327f
Amey Parundekar <parundekaramey@gmail.com>
2017-03-25 10:07:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/ui/:fileName', function (req, res) {
 //sql stuff
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-   pool.query('SELECT * FROM test', function(req,res,err){
+   pool.query('SELECT * FROM test', function(result,err){
        if(err){
            res.status(500).send(err.toString());
        }else{
