fd8166145c0f0b7fed92e9d22be2b2265f9fefb5
Amey Parundekar <parundekaramey@gmail.com>
2017-03-25 20:04:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/articles/:articleName',function(req,res){
   });
 });
 
-function hash(input){
+function hash(input,salt){
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
